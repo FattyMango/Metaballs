bfa8cb28eba3e1e0a83072dfde4342c400fee085
FattyMango <malak.jkl@gmail.com>
2024-05-03 00:56:36 +0300

    new server

--- html/server.go
+++ html/server.go
@@ -20,5 +20,5 @@ var Handler = http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "application/wasm")
 	w.Header().Set("Access-Control-Allow-Origin", "*")
 
-	http.ServeFile(w, r, "/home/kassab/GO/metaballs/html/metaballs.wasm")
+	http.ServeFile(w, r, "./metaballs.wasm")
 })
